8ac187427ceb675f293bdedcaf4fb58dec4109c0
patriceac <48073125+patriceac@users.noreply.github.com>
2023-04-28 12:51:06 -0700

    Add files via upload

--- plugins/Patrice/queue-size.plugin.js
+++ plugins/Patrice/queue-size.plugin.js
@@ -31,8 +31,34 @@
         }
         switch(event.type) {
             case 'online':
-                serverStatusMsg2.style.color = 'green'
-                serverStatusMsg2.innerText = 'Easy Diffusion is ' + event.message
+                // sometimes the server pretends to be ready while there are still entries to be processed
+                if (event.message.startsWith('ready')) {
+    				let taskCount = 0
+                    let imageCount = 0
+                    for (const taskEntry of getUncompletedTaskEntries()) {
+                        const task = htmlTaskMap.get(taskEntry)
+                        if (task) {
+                            taskCount += 1
+                            const imageCountTotal = (Math.floor(task.numOutputsTotal / task.reqBody.num_outputs) + (task.numOutputsTotal % task.reqBody.num_outputs ==  0 ? 0 : 1)) * task.reqBody.num_outputs
+                            const imageCountDone = ((task.batchesDone - (taskCount > 1 ? 0 : 1)) * task.reqBody.num_outputs) // batchesDone is initialized at 1 for the first task, but 0 for subsequent tasks, so we need to compensate here
+                            imageCount += imageCountTotal -  imageCountDone
+                        }
+                    }
+                    if (imageCount > 0) {
+                        serverStatusMsg2.style.color = 'rgb(200, 139, 0)'
+                        serverStatusMsg2.innerText = `Stable Diffusion is rendering* (${taskCount} active task${taskCount > 1 ? 's' : ''}, ${imageCount} image${imageCount > 1 ? 's' : ''} left)`
+                    }
+                    else
+                    {
+                        serverStatusMsg2.style.color = 'green'
+                        serverStatusMsg2.innerText = 'Stable Diffusion is ' + event.message
+                    }
+                }
+                else
+                {
+                    serverStatusMsg2.style.color = 'green'
+                    serverStatusMsg2.innerText = 'Stable Diffusion is ' + event.message
+                }
                 break
             case 'busy':
                 serverStatusMsg2.style.color = 'rgb(200, 139, 0)'
@@ -44,20 +70,20 @@
                         if (task) {
                             taskCount += 1
                             const imageCountTotal = (Math.floor(task.numOutputsTotal / task.reqBody.num_outputs) + (task.numOutputsTotal % task.reqBody.num_outputs ==  0 ? 0 : 1)) * task.reqBody.num_outputs
-                            const imageCountDone = ((task.batchesDone - (taskCount > 1 ? 0 : 1)) * task.reqBody.num_outputs) // batchesDone is initialized at 1 for the first task, but 0 for subsequent tasks, so we need tom compensate here 🤷
+                            const imageCountDone = ((task.batchesDone - (taskCount > 1 ? 0 : 1)) * task.reqBody.num_outputs) // batchesDone is initialized at 1 for the first task, but 0 for subsequent tasks, so we need to compensate here
                             imageCount += imageCountTotal -  imageCountDone
                         }
                     }
-                    serverStatusMsg2.innerText = `Easy Diffusion is ${event.message} (${taskCount} active task${taskCount > 1 ? 's' : ''}, ${imageCount} image${imageCount > 1 ? 's' : ''} left)`
+                    serverStatusMsg2.innerText = `Stable Diffusion is ${event.message} (${taskCount} active task${taskCount > 1 ? 's' : ''}, ${imageCount} image${imageCount > 1 ? 's' : ''} left)`
                 }
                 else
                 {
-                    serverStatusMsg2.innerText = 'Easy Diffusion is ' + event.message
+                    serverStatusMsg2.innerText = 'Stable Diffusion is ' + event.message
                 }
                 break
             case 'error':
                 serverStatusMsg2.style.color = 'red'
-                serverStatusMsg2.innerText = 'Easy Diffusion has stopped'
+                serverStatusMsg2.innerText = 'Stable Diffusion has stopped'
                 break
         }
     }
