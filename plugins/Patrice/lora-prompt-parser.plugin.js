8cd5d3d1bce8146954cde6b50e27b2aeba46cecf
patriceac <48073125+patriceac@users.noreply.github.com>
2023-05-31 00:24:24 -0700

    Fix LoRA case sensitivity and empty weights

--- plugins/Patrice/lora-prompt-parser.plugin.js
+++ plugins/Patrice/lora-prompt-parser.plugin.js
@@ -10,8 +10,6 @@
     promptField.addEventListener('input', function(e) {
         const { LoRA, prompt } = extractLoraTags(e.target.value);
     
-        console.log(prompt, LoRA);
-    
         if (LoRA !== null && LoRA.length > 0) {
             promptField.value = prompt.replace(/,+$/, ''); // remove any trailing ,
     
@@ -21,67 +19,74 @@
         }
                                  
         if (LoRA !== null && LoRA.length > 0 && testDiffusers?.checked) {
-            if (isStringInArray(modelsCache.options.lora, LoRA[0].loraname)) {
-                if (loraModelField.value !== LoRA[0].loraname) {
-                    // Set the new LoRA value
-                    loraModelField.value = LoRA[0].loraname;
-                    loraAlphaField.value = (LoRA[0].weight / 1.0).toFixed(2) || 0.5;
-                    loraAlphaSlider.value = loraAlphaField.value * 100;
-                    //TBD.value = LoRA[0].blockweights; // block weights not supported by ED at this time
-                    showToast("Prompt successfully processed")
-                }
-            }
-            else
-            {
-                showToast("LoRA not found: " + LoRA[0].loraname, 5000, true)
-            }
+            //if (loraModelField.value !== LoRA[0].lora_model) {
+                // Set the new LoRA value
+                loraModelField.value = LoRA[0].lora_model;
+                loraAlphaField.value = LoRA[0].lora_alpha;
+                loraAlphaSlider.value = loraAlphaField.value * 100;
+                //TBD.value = LoRA[0].blockweights; // block weights not supported by ED at this time
+            //}
+            showToast("Prompt successfully processed", LoRA[0].lora_model)
         }
             
         //promptField.dispatchEvent(new Event('change'));
     });
     
-    function isStringInArray(array, searchString) {
-        return array.some(function(item) {
+    function isModelAvailable(array, searchString) {
+        const foundItem = array.find(function(item) {
             return item.toLowerCase() === searchString.toLowerCase();
         });
+
+        return foundItem || "";
     }
-    
+
     // extract LoRA tags from strings
     function extractLoraTags(prompt) {
         // Define the regular expression for the tags
-        const regex = /<lora:([^:>]+)(?::([^:>]*))?(?::([^:>]*))?>/gi;
-    
+        const regex = /<lora:([^:>]+)(?::([^:>]*))?(?::([^:>]*))?>/gi
+
         // Initialize an array to hold the matches
-        let matches = [];
-    
+        let matches = []
+
         // Iterate over the string, finding matches
         for (const match of prompt.matchAll(regex)) {
-            // Initialize an object to hold a match
-            let loraTag = {
-                loraname: match[1].trim(),
-            };
-    
-            // If weight is provided, add it to the loraTag object
-            if (match[2] !== undefined && match[2] !== '') {
-                loraTag.weight = parseFloat(match[2].trim());
+            const modelFileName = isModelAvailable(modelsCache.options.lora, match[1].trim())
+            if (modelFileName !== "") {
+                // Initialize an object to hold a match
+                let loraTag = {
+                    lora_model: modelFileName,
+                }
+        
+                // If weight is provided, add it to the loraTag object
+                if (match[2] !== undefined && match[2] !== '') {
+                    loraTag.lora_alpha = parseFloat(match[2].trim())
+                }
+                else
+                {
+                    loraTag.lora_alpha = 0.5
+                }
+        
+                // If blockweights are provided, add them to the loraTag object
+                if (match[3] !== undefined && match[3] !== '') {
+                    loraTag.blockweights = match[3].trim()
+                }
+        
+                // Add the loraTag object to the array of matches
+                matches.push(loraTag)
             }
-    
-            // If blockweights are provided, add them to the loraTag object
-            if (match[3] !== undefined && match[3] !== '') {
-                loraTag.blockweights = match[3].trim();
+            else
+            {
+                showToast("LoRA not found: " + match[1].trim(), 5000, true)            
             }
-    
-            // Add the loraTag object to the array of matches
-            matches.push(loraTag);
         }
-    
-        // Clean up the prompt string
-        let cleanedImageTag = prompt.replace(regex, '').trim();
-    
+
+        // Clean up the prompt string, e.g. from "apple, banana, <lora:...>, orange, <lora:...>  , pear <lora:...>, <lora:...>" to "apple, banana, orange, pear"
+        let cleanedPrompt = prompt.replace(regex, '').replace(/(\s*,\s*(?=\s*,|$))|(^\s*,\s*)|\s+/g, ' ').trim();
+
         // Return the array of matches and cleaned prompt string
         return {
             LoRA: matches,
-            prompt: cleanedImageTag
-        };
+            prompt: cleanedPrompt
+        }
     }
 })()
